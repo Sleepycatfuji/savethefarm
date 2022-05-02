b434e4bf5e0d396924bff9e04c6ec7070c99c11e
Kim Methaullachat <blufairie888@gmail.com>
2022-05-02 13:32:31 +0900

    complete

--- changeLocation.php
+++ changeLocation.php
@@ -1,9 +1,37 @@
 <?php
   // Change player location
 function changeLocation(){
-  // Write your code here:
+  global $location;
 
+  echo "Where do you want to go?\n";
 
+  $destination = readline(">> ");
+  $destination = strtolower($destination);
 
-
+  if($location === "kitchen" && $destination === "bathroom") {
+    echo "You go to: bathroom\n";
+    $location = $destination;
+  }
+  elseif($location === "kitchen" && $destination === "woods") {
+    echo "You follow the winding path, shivering as you make your way deep into the Terror Woods.\n";
+    $location = $destination;
+  }
+  elseif($location === "bathroom" && $destination === "kitchen"){
+    echo "You go to: kitchen.\n";
+    $location = $destination;
+  }
+  elseif($location === "woods" && $destination === "kitchen"){
+    echo "You go to: kitchen.\n";
+    $location = $destination;
+  }
+  elseif($location === "woods" && $destination === "kitchen"){
+    echo "You go to: kitchen\n";
+    $location = $destination;
+  }
+  elseif($destination === "woods" || $destination === "kitchen" || $destination === "bathroom"){
+    echo "You can't go directly to $destination. Try going somewhere else first.\n";
+  }
+  else{
+    echo "That doesn't make sense. Are you confused? Try 'look around'.\n";
+  }
 }
--- printStatus.php
+++ printStatus.php
@@ -3,11 +3,38 @@
 function printStatus(){
     global $location, $wearing_glasses, $wearing_contacts, $has_mushrooms, $has_soup, $is_hungry, $needs_to_pee;
 
-    echo "You are in: $location\n";
-    if($wearing_glasses){
-    echo "You are wearing_glasses\n";
+   echo "You are in: $location\n";
+    if($wearing_glasses)
+    {
+    echo "You are wearing glasses\n";
     }
-    if($wearing_contacts){
-    echo "You are wearing_contacts\n";
+    else {
+      echo "You are not wearing glasses\n";
+      }
+    if($wearing_contacts)
+    {
+    echo "You are wearing contacts\n";
+    }
+    else
+    {
+      echo "You are not wearing contacts\n";
+    }
+     if($has_mushrooms)
+     {
+      echo "You are holding mushrooms\n";
+    }
+    if($has_soup)
+    {
+      echo "You are holding soup\n";
+    }
+    if($is_hungry)
+    {
+      echo "You are hungry\n";
+    }
+    else {
+      echo "You are well fed and energetic\n";
+    }
+    if($needs_to_pee){
+      echo "You are need to pee\n";
     }
 }
